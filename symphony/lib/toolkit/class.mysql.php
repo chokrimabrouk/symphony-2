93cd3a5d8c2128b675abbcabb3607fd95b83c4ca
Nils Werner <nils.werner@gmail.com>
2011-10-20 14:20:44 +0200

    Enable ASSOC/OBJECT to be passed as second parameter too

--- symphony/lib/toolkit/class.mysql.php
+++ symphony/lib/toolkit/class.mysql.php
@@ -432,6 +432,10 @@
 		 *  True if the query executed without errors, false otherwise
 		 */
 		public function query($query, $params = array(), $type = "OBJECT"){
+			if($params == "ASSOC" || $params == "OBJECT") {
+				$type = $params;
+				$params = array();
+			}
 
 			if(empty($query) || $this->isConnected() === false) return false;
 
