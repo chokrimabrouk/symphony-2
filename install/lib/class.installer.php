481ccadf445ad00a81ba41aaf13abae189d0ead7
Brendan Abbott <brendan@bloodbone.ws>
2013-10-18 21:31:15 +1000

    Cleanup of the character set now that PDO has it's own way of doing things

--- install/lib/class.installer.php
+++ install/lib/class.installer.php
@@ -426,7 +426,6 @@
 
 			// MySQL: Setting prefix & character encoding
 			Symphony::Database()->setPrefix($fields['database']['tbl_prefix']);
-			Symphony::Database()->setCharacterSet();
 
 			// MySQL: Importing schema
 			Symphony::Log()->pushToLog('MYSQL: Importing Table Schema', E_NOTICE, true, true);
